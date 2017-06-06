bf4e95436ec795e7cbea856ea6cb2615cb1a4cbe
TuxCoder <git@o-g.at>
2017-06-06 17:23:07 +0200

    rewrite spi driver

--- bibliopixel/drivers/spi_driver_base.py
+++ bibliopixel/drivers/spi_driver_base.py
@@ -1,75 +1,109 @@
-import os, sys, time
+import os
 from . channel_order import ChannelOrder
 from . driver_base import DriverBase
 from .. import log
 
 
-class DriverSPIBase(DriverBase):
-    """
-    Base driver for controling SPI devices on systems like the Raspberry Pi and
-    BeagleBone.
-    """
-    def __init__(self, num, c_order=ChannelOrder.GRB, use_py_spi=True,
-                 dev='/dev/spidev0.0', SPISpeed=2, open=open, gamma=None):
-        super().__init__(num, c_order=c_order, gamma=gamma)
+class SpiBaseInterface(object):
+    """ abstract class for different spi backends"""
 
-        self.dev = dev
-        self.use_py_spi = use_py_spi
-        self._spiSpeed = SPISpeed
-        self._open = open
+    def __init__(self, dev, SPISpeed):
+        self._dev = dev
+        self._spi_speed = SPISpeed
+        a, b = -1, -1
+        d = self._dev.replace("/dev/spidev", "")
+        s = d.split('.')
+        if len(s) == 2:
+            a = int(s[0])
+            b = int(s[1])
+        if a < 0 or b < 0:
+            error(BAD_FORMAT_ERROR)
 
-        # File based SPI requires a bytearray so we have to overwrite _buf
-        if self.use_py_spi:
-            a, b = -1, -1
-            d = self.dev.replace('/dev/spidev', '')
-            s = d.split('.')
-            if len(s) == 2:
-                a = int(s[0])
-                b = int(s[1])
+        self._device_id = a
+        self._device_cs = b
 
-            if a < 0 or b < 0:
-                error(BAD_FORMAT_ERROR)
+    def send_packet(self, data):
+        pass
 
-            self._bootstrapSPIDev()
-            import spidev
-            self.spi = spidev.SpiDev()
-            self.spi.open(a, b)
-            self.spi.max_speed_hz = int(self._spiSpeed * 1000000.0)
-            log.info('py-spidev speed @ %.1f MHz',
-                     (float(self.spi.max_speed_hz) / 1000000.0))
-        else:
-            self._buf = bytearray(self._buf)
-            self.spi = self._open(self.dev, 'wb')
+    def compute_packet(self, data):
+        return data
 
-    def _bootstrapSPIDev(self):
-        if self.use_py_spi:
-            try:
-                import spidev
-            except:
-                error(CANT_IMPORT_SPIDEV_ERROR)
 
-        if not os.path.exists(self.dev):
-            error(CANT_FIND_ERROR)
+class SpiFileInterface(SpiBaseInterface):
+    """ using os open/write to send data"""
 
+    def __init__(self, **kwargs):
+        super(SpiFileInterface, self).__init__(**kwargs)
+        self.spi = open(self._dev, "wb")
+
+    def send_packet(self, data):
+        self.spi.write(bytearray(data))
+        self.spi.flush()
+
+
+class SpiPyDevInterface(SpiBaseInterface):
+    """ using py-spidev to send data"""
+
+    def __init__(self, **kwargs):
+        super(SpiPyDevInterface, self).__init__(**kwargs)
+
+        if not os.path.exists(self._dev):
+            error(BAD_FORMAT_ERROR)
         # permissions check
         try:
-            self._open(self.dev)
+            fd = open(self._dev, 'r')
+            fd.close()
         except IOError as e:
             if e.errno == 13:
-                error(CANT_FIND_ERROR)
-            raise e
+                error(PERMISSION_ERROR)
+            else:
+                raise e
+        # import spidev and cache error
+        try:
+            import spidev
+            self.spi = spidev.SpiDev()
+        except:
+            error(CANT_IMPORT_SPIDEV_ERROR)
+        self.spi.open(self._device_id, self._device_cs)
+        self.spi.max_speed_hz = int(self._spi_speed * 1000000.0)
+        log.info('py-spidev speed @ %.1f MHz', (float(self.spi.max_speed_hz) / 1000000.0))
+
+    def _send_packet(self, data):
+        self.spi.xfer2(data)
+
+
+class SpiDummyInterface(SpiBaseInterface):
+    """ interface for testing proposal"""
+
+    def __init__(self, **kwargs):
+        super(SpiDummyInterface, self).__init__(**kwargs)
+        pass
+
+
+class DriverSPIBase(DriverBase):
+    """Base driver for controling SPI devices on systems like the Raspberry Pi and BeagleBone"""
+
+    def __init__(self, num, c_order=ChannelOrder.GRB, interface=SpiPyDevInterface,
+                 dev="/dev/spidev0.0", SPISpeed=2, gamma=None):
+        super(DriverSPIBase, self).__init__(num, c_order=c_order, gamma=gamma)
+
+        self.interface = interface(dev=dev, SPISpeed=SPISpeed)
 
     def _send_packet(self):
-        if self.use_py_spi:
-            self.spi.xfer2(self._packet)
-        else:
-            self.spi.write(self._packet)
-            self.spi.flush()
+        self.interface.send_packet(self._packet)
 
     def _compute_packet(self):
         self._render()
-        self._packet = self._buf
+        self._packet = self.interface.compute_packet(self._buf)
 
+
+PERMISSION_ERROR = """Cannot access SPI device.
+Please see
+
+    https://github.com/maniacallabs/bibliopixel/wiki/SPI-Setup
+
+for details.
+"""
 
 CANT_FIND_ERROR = """Cannot find SPI device.
 Please see
